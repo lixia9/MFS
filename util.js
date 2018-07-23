cf5467fcdc568c7a972f0c3c401170de107d7ff6
lixiaolong <lixiaolong@lixiaolong.local>
2018-07-23 13:20:20 +0800

    提交常用函数

--- util.js
+++ util.js
@@ -1,44 +1,54 @@
 //cookie 获取
-export let getCookie =(name)=> {
+export let getCookie = (name) => {
     var arr = document.cookie.match(new RegExp("(^| )" + name + "=([^;]*)(;|$)"));
     if (arr != null) return unescape(arr[2]);
     return null
 }
 //cookie 设置
-export let setCookie =(name, value, Hours,domain)=> {
+export let setCookie = (name, value, Hours, domain) => {
     var d = new Date();
     var offset = 8;
     var utc = d.getTime() + (d.getTimezoneOffset() * 60000);
     var nd = utc + (3600000 * offset);
     var exp = new Date(nd);
     exp.setTime(exp.getTime() + Hours * 60 * 60 * 1000);
-    document.cookie = name + "=" + escape(value) + ";path=/;expires=" + exp.toGMTString() + ";domain="+domain+";"
+    document.cookie = name + "=" + escape(value) + ";path=/;expires=" + exp.toGMTString() + ";domain=" + domain + ";"
 }
 
 // 用法：getUrlParam("lxl")
-export let  getUrlParam=(name,url=window.location.search)=>{ 
-    var reg = new RegExp("(^|&)"+name+"=([^&]*)(&|$)"); 
-    var r =  url.substr(1).match(reg);
+export let getUrlParam = (name, url = window.location.search) => {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+    var r = url.substr(1).match(reg);
     var strValue = "";
-    if (r!=null){
-     strValue= unescape(r[2]);
+    if (r != null) {
+        strValue = unescape(r[2]);
     }
     return strValue;
 }
 //判断是不是Android浏览器
-export let isAndroidMobileDevice=()=>{
+export let isAndroidMobileDevice = () => {
     return (/android/i.test(navigator.userAgent.toLowerCase()));
 }
 //判断是不是IOS浏览器
-export let isIosMobileDevice=()=>{
-    return(/iPhone|iPad/i.test(navigator.userAgent.toLowerCase()))
+export let isIosMobileDevice = () => {
+    return (/iPhone|iPad/i.test(navigator.userAgent.toLowerCase()))
+}
+
+//判断是否是移动设备访问
+export let isMobileUserAgent = () => {
+    return (/iphone|ipod|android.*mobile|windows.*phone|blackberry.*mobile/i.test(window.navigator.userAgent.toLowerCase()));
+}
+//判断是否为网址
+export let isURL = (strUrl) => {
+    var regular = /^\b(((https?|ftp):\/\/)?[-a-z0-9]+(\.[-a-z0-9]+)*\.(?:com|edu|gov|int|mil|net|org|biz|info|name|museum|asia|coop|aero|[a-z][a-z]|((25[0-5])|(2[0-4]\d)|(1\d\d)|([1-9]\d)|\d))\b(\/[-a-z0-9_:\@&?=+,.!\/~%\$]*)?)$/i
+    return regular.test(strUrl)
 }
 
 //浏览器复制
-export let copyToClipboard=(text)=>{
+export let copyToClipboard = (text) => {
     if (window.clipboardData && window.clipboardData.setData) {
         // IE specific code path to prevent textarea being shown while dialog is visible.
-        return clipboardData.setData("Text", text); 
+        return clipboardData.setData("Text", text);
 
     } else if (document.queryCommandSupported && document.queryCommandSupported("copy")) {
         var textarea = document.createElement("textarea");
@@ -55,35 +65,40 @@ export let copyToClipboard=(text)=>{
             document.body.removeChild(textarea);
         }
     }
-  }
-  //获取浏览器选择段落文本对象
-  export let getSelectedText =()=> {
+}
+//获取浏览器选择段落文本对象
+export let getSelectedText = () => {
     var t = '';
-      if (window.getSelection) {
-          t = window.getSelection();
-      } else if (document.getSelection) {
-          t = document.getSelection();
-      } else if (document.selection) {
-          t = document.selection.createRange().text;
-      }
-      return t;
-  }
+    if (window.getSelection) {
+        t = window.getSelection();
+    } else if (document.getSelection) {
+        t = document.getSelection();
+    } else if (document.selection) {
+        t = document.selection.createRange().text;
+    }
+    return t;
+}
 //日期转换 new Date().format("yyyy-MM-dd hh:mm:ss")
-Date.prototype.format = function(format){
+Date.prototype.format = function (format) {
     var o = {
-        "M+" : this.getMonth()+1, //month
-        "d+" : this.getDate(),    //day
-        "h+" : this.getHours(),   //hour
-        "m+" : this.getMinutes(), //minute
-        "s+" : this.getSeconds(), //second
-        "q+" : Math.floor((this.getMonth()+3)/3),  //quarter
-        "S" : this.getMilliseconds() //millisecond
+        "M+": this.getMonth() + 1, //month
+        "d+": this.getDate(),    //day
+        "h+": this.getHours(),   //hour
+        "m+": this.getMinutes(), //minute
+        "s+": this.getSeconds(), //second
+        "q+": Math.floor((this.getMonth() + 3) / 3),  //quarter
+        "S": this.getMilliseconds() //millisecond
     };
-    if(/(y+)/.test(format)) format=format.replace(RegExp.$1,
-(this.getFullYear()+"").substr(4 - RegExp.$1.length));
-    for(var k in o){
-        if(new RegExp("("+ k +")").test(format))
-            format = format.replace(RegExp.$1,RegExp.$1.length==1 ? o[k] :("00"+ o[k]).substr((""+ o[k]).length));
+    if (/(y+)/.test(format)) format = format.replace(RegExp.$1,
+        (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+    for (var k in o) {
+        if (new RegExp("(" + k + ")").test(format))
+            format = format.replace(RegExp.$1, RegExp.$1.length == 1 ? o[k] : ("00" + o[k]).substr(("" + o[k]).length));
     }
     return format;
+}
+
+ //replaceAll
+String.prototype.replaceAll = function(s1, s2) {
+    return this.replace(new RegExp(s1, "gm"), s2)
 }
